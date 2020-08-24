62c8883a6beba76361948e32249e6072b9389639
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-08-24 18:44:57 +0200

    LACE-401 | read() in activity_events ready for querying.
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/activity_events.py
+++ src/activity_events.py
@@ -1,20 +1,19 @@
-# from os import getenv
+import os
+
 from typing import Union
 
 from pathlib import Path
 from datetime import datetime
 
-from dotenv import load_dotenv
+import dotenv
 import datatosk
 import pandas as pd
 
 
-ENV_PATH = Path("..") / ".env"  # FIXME .. to .
-load_dotenv(dotenv_path=ENV_PATH)
+ENV_PATH = Path(".") / ".env"
+dotenv.load_dotenv(dotenv_path=ENV_PATH)
 
-
-# BIGQUERY_PLATFORM_DATASET_ID: str = getenv("BIGQUERY_DATASET_ID_WNL_PLATFORM", None)
-BIGQUERY_PLATFORM_DATASET_ID = "lek_platform"
+BIGQUERY_PLATFORM_DATASET_ID: str = os.getenv("BIGQUERY_PLATFORM_DATASET_ID", None)
 
 
 def read(
@@ -24,10 +23,11 @@ def read(
     bigquery_source = datatosk.gbq("prod")
 
     query = f"""
-        SELECT user_id, client_time
+        SELECT user_id, DATETIME(client_time) as client_time
         FROM `{BIGQUERY_PLATFORM_DATASET_ID}.user_activity_events`
         WHERE client_time BETWEEN @start_date AND @end_date 
-        ORDER BY user_id, client_time ASC;
+        GROUP BY user_id, client_time
+        ORDER BY user_id, client_time;
         """
 
     params = {
@@ -35,13 +35,14 @@ def read(
         "end_date": end_date,
     }
 
-    return bigquery_source.read(query=query, params=params).drop_duplicates()
+    return bigquery_source.read(query=query, params=params)
 
 
 if __name__ == "__main__":
-
+    # FIXME left for debugging, delete before releasing
     start = datetime.now()
-    pyk = read("2020-08-24", datetime.now())
+    pyk = read("2020-08-24 14:00:00", "2020-08-24 15:00:00")
     end = datetime.now()
     print(pyk)
+    print(pyk.info())
     print((end - start).total_seconds())
