3b59a3033b7b4f9ae14d4d2f14e0272e5a2aa26d
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-08-31 18:23:10 +0200

    LACE-400 | remove if __name__ == '__main__':
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/activity_events.py
+++ src/activity_events.py
@@ -17,7 +17,7 @@ BIGQUERY_PLATFORM_DATASET_ID: str = os.getenv("BIGQUERY_PLATFORM_DATASET_ID", No
 
 
 def read(
-    start_date: Union[datetime, str], end_date: Union[datetime, str]
+    start_time: Union[datetime, str], end_time: Union[datetime, str]
 ) -> pd.DataFrame:
 
     bigquery_source = datatosk.gbq("prod")
@@ -25,24 +25,14 @@ def read(
     query = f"""
         SELECT user_id, DATETIME(client_time) as client_time
         FROM `{BIGQUERY_PLATFORM_DATASET_ID}.user_activity_events`
-        WHERE client_time BETWEEN @start_date AND @end_date 
+        WHERE client_time BETWEEN @start_time AND @end_time 
         GROUP BY user_id, client_time
         ORDER BY user_id, client_time;
         """
 
     params = {
-        "start_date": start_date,
-        "end_date": end_date,
+        "start_time": start_time,
+        "end_time": end_time,
     }
 
     return bigquery_source.read(query=query, params=params)
-
-
-if __name__ == "__main__":
-    # FIXME left for debugging, delete before releasing
-    start = datetime.now()
-    pyk = read("2020-08-24 14:00:00", "2020-08-24 15:00:00")
-    end = datetime.now()
-    print(pyk)
-    print(pyk.info())
-    print((end - start).total_seconds())
