4b35230c2e6fe28bcd41c6bdf5ce2ee8d93aede2
andrysiaczek <agata.and.11@gmail.com>
2020-08-31 12:02:19 +0200

    LACE-447 | tests

---
+++ tests/api/test_get_user_learning_time_daily.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+from consts.api_tests import HEADERS, STATUS_OK
+from tests.api.url_helpers import get_url
+
+test_data = {
+    "start_date": 15934399493,
+    "end_date": 15934399490
+}
+
+user_id = 299
+
+
+def test_get_user_learning_time_daily():
+    response = requests.post(get_url(f"learning_time_daily/{user_id}"), headers=HEADERS, data=json.dumps(test_data))
+    assert response.status_code == STATUS_OK
+    assert response.json() == {
+        "2000-01-01": 100,
+        "2000-01-02": 300,
+        "2000-01-03": 250
+    }
