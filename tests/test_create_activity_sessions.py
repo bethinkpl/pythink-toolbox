b43677652d82bc1a8e52cb4b1a42aee8da9333c3
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-03 16:50:06 +0200

    draft tests
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

---
+++ tests/test_create_activity_sessions.py
@@ -0,0 +1,44 @@
+import python_toolbox.testing.parametrization as parametrization
+
+
+def test_main():
+    # FIXME
+    assert False
+
+
+def test__create_user_activity_sessions():
+    # FIXME
+    assert False
+
+
+def test__initialize_sessions_creation():
+
+    assert False
+
+
+def test__add_last_active_session():
+    assert False
+
+
+def test__create_active_sessions():
+    assert False
+
+
+def test__fill_with_inactive_sessions():
+    assert False
+
+
+def test__determine_if_focus():
+    assert False
+
+
+def test__determine_if_break():
+    assert False
+
+
+def test__sessions_validation():
+    assert False
+
+
+def test__to_dict():
+    assert False
