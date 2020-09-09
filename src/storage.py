44074089924ffb2e36977c95d974d4c3643a67d8
Kuba Karmiński <jlkarminski@gmail.com>
2020-09-09 13:21:32 +0200

    LACE-436 | Fix chex

--- src/storage.py
+++ src/storage.py
@@ -1,10 +1,10 @@
+import logging as log
 from pathlib import Path
 from typing import List
-import logging as log
 
 import datatosk
 import dotenv
-import pandas as pd
+import pandas as pd  # type: ignore[import]
 
 ENV_PATH = Path(".") / ".env"
 dotenv.load_dotenv(dotenv_path=ENV_PATH)
@@ -12,26 +12,35 @@ dotenv.load_dotenv(dotenv_path=ENV_PATH)
 mongo_source = datatosk.mongodb("chronos")
 
 
-def read_learning_time(user_ids: List) -> List:
+def read_learning_time(user_ids: List[int]) -> datatosk.types.ListType:
+    """
+    Read user learning time from mongodb.
+    """
     return mongo_source.read.to_list(
         collection="learning_time_daily_view",
-        result_filter={"user_id": {"$in": user_ids}},
+        query_filter={"user_id": {"$in": user_ids}},
     )
 
 
 def write_activity_sessions(activity_sessions: pd.DataFrame) -> None:
+    """
+    Write activity sessions to mongodb.
+    """
     mongo_source.write(collection="activity_sessions", data=activity_sessions)
-    log.info(f"Wrote {len(activity_sessions)} activity sessions to storage.")
+    log.info("Wrote %i activity sessions to storage.", len(activity_sessions))
 
 
 def read_activity_sessions_by_user(user_id: int) -> pd.DataFrame:
+    """
+    Read activity session from mongodb for a defined user.
+    """
     return mongo_source.read.to_pandas(
-        collection="activity_sessions", result_filter={"user_id": user_id},
+        collection="activity_sessions", query_filter={"user_id": user_id},
     )
 
 
 if __name__ == "__main__":
-    # FIXME Debug code, remove in production version.
+    # FIXME Debug code, remove in production version. pylint: disable=fixme
     # Learning time reading example
     print(f"read_learning_time: {read_learning_time([1])}")
 
