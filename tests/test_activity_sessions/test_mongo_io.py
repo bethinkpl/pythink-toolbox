1024d0fc86b9efccb9629a652145e39ffa4d9d9e
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-12 01:05:40 +0200

    LACE-466 | add tests steps
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- tests/test_activity_sessions/test_mongo_io.py
+++ tests/test_activity_sessions/test_mongo_io.py
@@ -12,7 +12,7 @@ def step_clear_activity_sessions_collection():
 
 def step_run_main():
     user_id = 500
-    activity_events = pd.Series([datetime(2000, 1, 1)])
+    activity_events = pd.Series([datetime(2000, 1, 1), datetime(2000, 1, 2)])
     start_time = datetime(2000, 1, 1)
     tested_module.main(
         user_id=user_id, activity_events=activity_events, start_time=start_time
@@ -20,27 +20,99 @@ def step_run_main():
 
 
 def step_check_data():
-    data = tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
-    data = list(data)[0]
-    data.pop("_id")
+    collection = tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
 
-    expected_data = {
-        "user_id": 500,
-        "start_time": datetime(1999, 12, 31, 23, 59),
-        "end_time": datetime(2000, 1, 1, 0, 0),
-        "is_active": True,
-        "is_focus": False,
-        "is_break": False,
-    }
+    data_in_collection = []
+    for document in collection:
+        data_in_collection.append(
+            {key: value for key, value in document.items() if key != "_id"}
+        )
 
-    assert data == expected_data
+    expected_data = [
+        {
+            "user_id": 500,
+            "start_time": datetime(1999, 12, 31, 23, 59),
+            "end_time": datetime(2000, 1, 1, 0, 0),
+            "is_active": True,
+            "is_focus": False,
+            "is_break": False,
+        },
+        {
+            "user_id": 500,
+            "start_time": datetime(2000, 1, 1, 0, 0),
+            "end_time": datetime(2000, 1, 1, 23, 59),
+            "is_active": False,
+            "is_focus": False,
+            "is_break": False,
+        },
+        {
+            "user_id": 500,
+            "start_time": datetime(2000, 1, 1, 23, 59),
+            "end_time": datetime(2000, 1, 2, 0, 0),
+            "is_active": True,
+            "is_focus": False,
+            "is_break": False,
+        },
+    ]
+
+    assert data_in_collection == expected_data
 
 
 def step_run_next_sessions_creation():
-    pass  # FIXME
+    user_id = 500
+    activity_events = pd.Series([datetime(2000, 1, 2, 0, 5)])
+    start_time = datetime(2000, 1, 2)
+    tested_module.main(
+        user_id=user_id, activity_events=activity_events, start_time=start_time
+    )
 
 
-@test_steps(step_clear_activity_sessions_collection, step_run_main, step_check_data)
+def step_check_data2():
+    collection = tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
+
+    data_in_collection = []
+    for document in collection:
+        data_in_collection.append(
+            {key: value for key, value in document.items() if key != "_id"}
+        )
+
+    expected_data = [
+        {
+            "user_id": 500,
+            "start_time": datetime(1999, 12, 31, 23, 59),
+            "end_time": datetime(2000, 1, 1, 0, 0),
+            "is_active": True,
+            "is_focus": False,
+            "is_break": False,
+        },
+        {
+            "user_id": 500,
+            "start_time": datetime(2000, 1, 1, 0, 0),
+            "end_time": datetime(2000, 1, 1, 23, 59),
+            "is_active": False,
+            "is_focus": False,
+            "is_break": False,
+        },
+        {
+            "user_id": 500,
+            "start_time": datetime(2000, 1, 1, 23, 59),
+            "end_time": datetime(2000, 1, 2, 0, 5),
+            "is_active": True,
+            "is_focus": False,
+            "is_break": False,
+        },
+    ]
+
+    assert data_in_collection == expected_data
+
+
+@test_steps(
+    step_clear_activity_sessions_collection,
+    step_run_main,
+    step_check_data,
+    step_run_next_sessions_creation,
+    step_check_data2,
+)
 def test_main(test_step):
     test_step()
 
