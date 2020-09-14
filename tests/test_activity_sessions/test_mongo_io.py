68fbe0addbe3de1f1db4ab13933e185ffcb305a6
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-14 15:20:26 +0200

    LACE-466 | Whole activity_sessions creation flow for one user with mongo io operations tested.
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- tests/test_activity_sessions/test_mongo_io.py
+++ tests/test_activity_sessions/test_mongo_io.py
@@ -1,122 +1,197 @@
+"""Whole activity_sessions creation flow for one user."""
+
 from datetime import datetime
+from typing import Dict, List
+import unittest.mock
 
-from pytest_steps import test_steps
 import pandas as pd
+import pymongo.cursor
+from pythink_toolbox.testing.parametrization import parametrize, Scenario
 
+from chronos.activity_sessions.create_activity_sessions import ActivitySession
 import chronos.activity_sessions.mongo_io as tested_module
 
 
-def step_clear_activity_sessions_collection():
-    tested_module.ACTIVITY_SESSIONS_COLLECTION.delete_many({})
+TEST_USER_ID = 1
 
 
-def step_run_main():
-    user_id = 500
-    activity_events = pd.Series([datetime(2000, 1, 1), datetime(2000, 1, 2)])
-    start_time = datetime(2000, 1, 1)
+tested_module.ACTIVITY_SESSIONS_COLLECTION.delete_many({})
+
+
+class MainScenario(Scenario):
+    activity_events: pd.Series
+    expected_data: List[ActivitySession]
+
+
+TEST_DATA = [
+    MainScenario(
+        desc="Initial input - creates two separate active sessions and inactive in the middle",
+        activity_events=pd.Series([datetime(2000, 1, 1), datetime(2000, 1, 2)]),
+        expected_data=[
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(1999, 12, 31, 23, 59),
+                "end_time": datetime(2000, 1, 1, 0, 0),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 0, 0),
+                "end_time": datetime(2000, 1, 1, 23, 59),
+                "is_active": False,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 23, 59),
+                "end_time": datetime(2000, 1, 2, 0, 0),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+        ],
+    ),
+    MainScenario(
+        desc="Takes last active session & extends its duration.",
+        activity_events=pd.Series([datetime(2000, 1, 2, 0, 5)]),
+        expected_data=[
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(1999, 12, 31, 23, 59),
+                "end_time": datetime(2000, 1, 1, 0, 0),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 0, 0),
+                "end_time": datetime(2000, 1, 1, 23, 59),
+                "is_active": False,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 23, 59),
+                "end_time": datetime(2000, 1, 2, 0, 5),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+        ],
+    ),
+    MainScenario(
+        desc="Takes last active session & extends its duration, so it changes to focus session.",
+        activity_events=pd.Series(
+            [datetime(2000, 1, 2, 0, 10), datetime(2000, 1, 2, 0, 15)]
+        ),
+        expected_data=[
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(1999, 12, 31, 23, 59),
+                "end_time": datetime(2000, 1, 1, 0, 0),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 0, 0),
+                "end_time": datetime(2000, 1, 1, 23, 59),
+                "is_active": False,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 23, 59),
+                "end_time": datetime(2000, 1, 2, 0, 15),
+                "is_active": True,
+                "is_focus": True,
+                "is_break": False,
+            },
+        ],
+    ),
+    MainScenario(
+        desc="Add new sessions one focused in the end and on that is 'break' before.",
+        activity_events=pd.Series(
+            [
+                datetime(2000, 1, 2, 0, 21, 1),
+                datetime(2000, 1, 2, 0, 25),
+                datetime(2000, 1, 2, 0, 30),
+                datetime(2000, 1, 2, 0, 35),
+                datetime(2000, 1, 2, 0, 40),
+            ]
+        ),
+        expected_data=[
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(1999, 12, 31, 23, 59),
+                "end_time": datetime(2000, 1, 1, 0, 0),
+                "is_active": True,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 0, 0),
+                "end_time": datetime(2000, 1, 1, 23, 59),
+                "is_active": False,
+                "is_focus": False,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 1, 23, 59),
+                "end_time": datetime(2000, 1, 2, 0, 15),
+                "is_active": True,
+                "is_focus": True,
+                "is_break": False,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 2, 0, 15),
+                "end_time": datetime(2000, 1, 2, 0, 20, 1),
+                "is_active": False,
+                "is_focus": False,
+                "is_break": True,
+            },
+            {
+                "user_id": TEST_USER_ID,
+                "start_time": datetime(2000, 1, 2, 0, 20, 1),
+                "end_time": datetime(2000, 1, 2, 0, 40),
+                "is_active": True,
+                "is_focus": True,
+                "is_break": False,
+            },
+        ],
+    ),
+]
+
+
+@parametrize(TEST_DATA)
+def test_main(activity_events: pd.Series, expected_data: List[ActivitySession]) -> None:
+
     tested_module.main(
-        user_id=user_id, activity_events=activity_events, start_time=start_time
+        user_id=TEST_USER_ID,
+        activity_events=activity_events,
+        start_time=unittest.mock.ANY,
     )
 
-
-def step_check_data():
-    collection = tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
-
-    data_in_collection = []
-    for document in collection:
-        data_in_collection.append(
-            {key: value for key, value in document.items() if key != "_id"}
-        )
-
-    expected_data = [
-        {
-            "user_id": 500,
-            "start_time": datetime(1999, 12, 31, 23, 59),
-            "end_time": datetime(2000, 1, 1, 0, 0),
-            "is_active": True,
-            "is_focus": False,
-            "is_break": False,
-        },
-        {
-            "user_id": 500,
-            "start_time": datetime(2000, 1, 1, 0, 0),
-            "end_time": datetime(2000, 1, 1, 23, 59),
-            "is_active": False,
-            "is_focus": False,
-            "is_break": False,
-        },
-        {
-            "user_id": 500,
-            "start_time": datetime(2000, 1, 1, 23, 59),
-            "end_time": datetime(2000, 1, 2, 0, 0),
-            "is_active": True,
-            "is_focus": False,
-            "is_break": False,
-        },
-    ]
-
-    assert data_in_collection == expected_data
-
-
-def step_run_next_sessions_creation():
-    user_id = 500
-    activity_events = pd.Series([datetime(2000, 1, 2, 0, 5)])
-    start_time = datetime(2000, 1, 2)
-    tested_module.main(
-        user_id=user_id, activity_events=activity_events, start_time=start_time
+    activity_sessions_collection_content = (
+        tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
     )
+    data = _filter_id_field(query_result=activity_sessions_collection_content)
+
+    assert data == expected_data
 
 
-def step_check_data2():
-    collection = tested_module.ACTIVITY_SESSIONS_COLLECTION.find()
-
-    data_in_collection = []
-    for document in collection:
-        data_in_collection.append(
-            {key: value for key, value in document.items() if key != "_id"}
-        )
-
-    expected_data = [
-        {
-            "user_id": 500,
-            "start_time": datetime(1999, 12, 31, 23, 59),
-            "end_time": datetime(2000, 1, 1, 0, 0),
-            "is_active": True,
-            "is_focus": False,
-            "is_break": False,
-        },
-        {
-            "user_id": 500,
-            "start_time": datetime(2000, 1, 1, 0, 0),
-            "end_time": datetime(2000, 1, 1, 23, 59),
-            "is_active": False,
-            "is_focus": False,
-            "is_break": False,
-        },
-        {
-            "user_id": 500,
-            "start_time": datetime(2000, 1, 1, 23, 59),
-            "end_time": datetime(2000, 1, 2, 0, 5),
-            "is_active": True,
-            "is_focus": False,
-            "is_break": False,
-        },
+def _filter_id_field(query_result: pymongo.cursor.Cursor) -> List[Dict]:
+    return [
+        {key: value for key, value in document.items() if key != "_id"}
+        for document in query_result
     ]
-
-    assert data_in_collection == expected_data
-
-
-@test_steps(
-    step_clear_activity_sessions_collection,
-    step_run_main,
-    step_check_data,
-    step_run_next_sessions_creation,
-    step_check_data2,
-)
-def test_main(test_step):
-    test_step()
-
-
-# initialize new collection
-# perform activity_sessions creations stuff
-# remove collection
