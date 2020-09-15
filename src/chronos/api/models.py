84433c7baaf00beb8b0e4a8d790aca61edfd14a1
andrysiaczek <agata.and.11@gmail.com>
2020-09-15 13:59:18 +0200

    LACE-464 | replace 'Users' model class by List[User]

--- src/chronos/api/models.py
+++ src/chronos/api/models.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from pydantic.main import BaseModel
 
@@ -7,7 +7,3 @@ class User(BaseModel):
     id: Optional[int] = None
     start_date: str
     end_date: str
-
-
-class Users(BaseModel):
-    users: List[User]
--- src/chronos/api/users_learning_time.py
+++ src/chronos/api/users_learning_time.py
@@ -1,21 +1,23 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from chronos.api.user_learning_time import get_learning_time
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 
-from chronos.api.models import Users
+from chronos.api.models import User
 
 users_learning_router = APIRouter()
 
 
 # TODO: Try to improve the return type hint after FastAPI is introduced.
 @users_learning_router.post("/learning_time")
-def get_users_learning_time(users: Users) -> Dict[Any, Any]:
+def get_users_learning_time(
+    users: List[User] = Body(..., embed=True)
+) -> Dict[Any, Any]:
     """
     API end-point | Provides cumulative learning time for a group of users.
     """
     learning_times = {
         user.id: get_learning_time(user.id, user.start_date, user.end_date)
-        for user in users.users
+        for user in users
     }
     return learning_times
