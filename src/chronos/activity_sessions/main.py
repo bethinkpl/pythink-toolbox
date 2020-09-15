80a9fb6b807f59a17c7dcc2e8a568f4dcb97fff1
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-15 16:10:22 +0200

    LACE-466 | add logger
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/activity_sessions/main.py
+++ src/chronos/activity_sessions/main.py
@@ -1,10 +1,13 @@
 # TODO notes: handle how to constrain dates of new queries to read_activity_events and materialized_views
 from datetime import datetime
+import logging
 
 from tqdm import tqdm  # type: ignore[import]
 
 import chronos.activity_sessions.create_activity_sessions
 import chronos.activity_sessions.mongo_io
+
+logger = logging.getLogger(__name__)
 
 
 def main(start_time: datetime, end_time: datetime) -> None:
@@ -15,17 +18,13 @@ def main(start_time: datetime, end_time: datetime) -> None:
         start_time=start_time, end_time=end_time
     )
 
+    logger.info(
+        "Creating activity sessions from %i activity_events", len(users_activity_events)
+    )
+
     users_activity_events_groups = users_activity_events.groupby("user_id").client_time
 
     for user_id, activity_events in tqdm(users_activity_events_groups):
         chronos.activity_sessions.mongo_io.main(
             user_id, activity_events, start_time=start_time
         )
-
-
-if __name__ == "__main__":
-    # FIXME move to cli.py
-    start_time_ = datetime(2000, 1, 1)
-    end_time_ = datetime(2000, 1, 1)
-
-    main(start_time=start_time_, end_time=end_time_)
