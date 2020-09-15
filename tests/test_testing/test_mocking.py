a20eacb80a88b11356d856710b2b3d7107715f4e
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-15 12:02:27 +0200

    Add tests
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

---
+++ tests/test_testing/test_mocking.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+from typing import Any, Callable
+
+import pytest
+
+import pythink_toolbox.testing.mocking
+from pythink_toolbox.testing.parametrization import parametrize
+import pythink_toolbox.validating
+
+
+@pytest.mark.parametrize(  # type: ignore[misc]
+    "function, expected_output",
+    [
+        (parametrize, "pythink_toolbox.testing.parametrization.parametrize"),
+        (
+            pythink_toolbox.validating.check_output,
+            "pythink_toolbox.validating.check_output",
+        ),
+    ],
+)
+def test_function_to_mock_target(
+    function: Callable[..., Any], expected_output: str
+) -> None:
+    output = pythink_toolbox.testing.mocking.transform_function_to_target_string(
+        function
+    )
+    assert expected_output == output
