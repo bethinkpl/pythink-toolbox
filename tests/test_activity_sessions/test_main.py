3305ad3364fb9e4b1bf1ed6e9decc6013f2ba345
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-15 16:27:30 +0200

    LACE-466 | add test for chronos/activity_sessions/main.py
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- tests/test_activity_sessions/test_main.py
+++ tests/test_activity_sessions/test_main.py
@@ -1,6 +1,59 @@
+# pylint: disable=missing-function-docstring
+
+from datetime import datetime
+from typing import Callable, List, Dict, Union
+
 import pytest
+from pytest_mock import MockerFixture
+from pythink_toolbox.testing.mocking import transform_function_to_target_string
+import pandas as pd
 
+import chronos.activity_sessions.main
+from chronos.activity_sessions.activity_events import read
+import chronos.settings
 
-@pytest.mark.integration
-def test_main():
-    assert False
+# TODO When GBQ integration ready -> replace mock/add new test
+@pytest.mark.integration  # type: ignore[misc]
+def test_main(
+    mocker: MockerFixture,
+    get_activity_session_collection_content_without_id: Callable[
+        [], List[Dict[str, Union[int, datetime, bool]]]
+    ],
+) -> None:
+
+    mocker.patch(
+        transform_function_to_target_string(read),
+        return_value=pd.DataFrame(
+            columns=["user_id", "client_time"],
+            data=[
+                [1, datetime(2000, 1, 1, 0, 1)],
+                [1, datetime(2000, 1, 1, 0, 2)],
+                [2, datetime(2000, 1, 1, 0, 1)],
+            ],
+        ),
+    )
+
+    chronos.activity_sessions.main.main(start_time=mocker.ANY, end_time=mocker.ANY)
+
+    data = get_activity_session_collection_content_without_id()
+
+    expected_data = [
+        {
+            "user_id": 1,
+            "start_time": datetime(2000, 1, 1),
+            "end_time": datetime(2000, 1, 1, 0, 2),
+            "is_active": True,
+            "is_break": False,
+            "is_focus": False,
+        },
+        {
+            "user_id": 2,
+            "start_time": datetime(2000, 1, 1),
+            "end_time": datetime(2000, 1, 1, 0, 1),
+            "is_active": True,
+            "is_break": False,
+            "is_focus": False,
+        },
+    ]
+
+    assert data == expected_data
