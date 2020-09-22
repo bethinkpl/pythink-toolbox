e9b9765486b5fae4f572e16773b07c11e6d86a74
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-22 19:23:35 +0200

    LACE-459 | materialized view draft
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/activity_sessions/storage_operations.py
+++ src/chronos/activity_sessions/storage_operations.py
@@ -5,8 +5,9 @@ from datetime import datetime
 import bson
 import pandas as pd
 import pymongo
-from pymongo.client_session import ClientSession
 import pymongo.errors
+from pymongo.client_session import ClientSession
+from pymongo.collection import Collection
 
 import chronos.activity_sessions
 from chronos.storage import get_client, get_activity_sessions_collection
@@ -14,7 +15,7 @@ from chronos.storage import get_client, get_activity_sessions_collection
 logger = logging.getLogger(__name__)
 
 
-class MongoCommitError(Exception):
+class _MongoCommitError(Exception):
     """Error that occurred while performing MongoDB commit."""
 
 
@@ -23,25 +24,32 @@ def main(user_id: int, activity_events: pd.Series, reference_time: datetime) -> 
 
     logger.info("Run test_activity_sessions mongo operations for user_id %i", user_id)
 
+    collection = get_activity_sessions_collection()
+
     with get_client().start_session() as session:
         try:
             _run_user_crud_operations_transaction(
                 user_id=user_id,
                 activity_events=activity_events,
                 session=session,
+                collection=collection,
             )
-        except MongoCommitError:
-            pass  # TODO LACE-471
+        except _MongoCommitError:
+            raise  # TODO LACE-471
 
-        _run_materialized_views_update(session=session, reference_time=reference_time)
+        _run_materialized_views_update(
+            reference_time=reference_time, collection=collection
+        )
 
 
 def _run_user_crud_operations_transaction(
-    user_id: int, activity_events: pd.Series, session: ClientSession
+    user_id: int,
+    activity_events: pd.Series,
+    session: ClientSession,
+    collection: Collection,
 ) -> None:
 
     with session.start_transaction(write_concern=pymongo.WriteConcern(w="majority")):
-        collection = get_activity_sessions_collection()
 
         last_active_session: Optional[
             Dict[str, Union[datetime, bson.ObjectId]]
@@ -83,17 +91,74 @@ def _commit_transaction_with_retry(session: ClientSession) -> None:
                 )
                 continue
 
-            raise MongoCommitError(
+            raise _MongoCommitError(
                 "Error during test_activity_sessions creation transaction commit."
             ) from err
 
 
 def _run_materialized_views_update(
-    session: ClientSession, reference_time: datetime  # pylint: disable=unused-argument
+    reference_time: datetime, collection: Collection
 ) -> None:
 
     # maybe asynchronous update materialized view after every user?
     #   materialized_views = [view1, view2, view3, view4]
     #   for materialized_view in materialized_views:
     #       update_materialized_view(materialized_view=materialized_view, start_time=start_time)
-    pass  # TODO LACE-459
+
+    match_step = {
+        "$match": {
+            "$or": [{"is_active": {"$eq": True}}, {"is_break": {"$eq": True}}],
+            "start_time": {"$gte": reference_time},
+        }
+    }
+
+    group_step = {
+        "$group": {
+            "_id": {
+                "user_id": "$user_id",
+                "date_hour": {
+                    "$dateToString": {"format": "%Y-%m-%d %H", "date": "$start_time"}
+                },  # FIXME cannot just cast start_time to string - this leads to more than one hour aggregated times
+            },
+            "duration_ms": {"$sum": {"$subtract": ["$end_time", "$start_time"]}},
+        }
+    }
+
+    project_step = {
+        "$project": {
+            "_id": bson.ObjectId(),
+            "user_id": "$_id.user_id",
+            "time_range": {
+                "start": {
+                    "$dateFromString": {
+                        "dateString": "$_id.date_hour",
+                        "format": "%Y-%m-%d %H",
+                    }
+                },
+                "end": {
+                    "$add": [
+                        {
+                            "$dateFromString": {
+                                "dateString": "$_id.date_hour",
+                                "format": "%Y-%m-%d %H",
+                            }
+                        },
+                        60 * 60 * 1000,
+                    ]
+                },
+            },
+            "duration_ms": 1,
+        }
+    }
+    merge_step = {
+        "$merge": {"into": "daily_learning_time_mv", "whenMatched": "replace"}
+    }
+
+    collection.aggregate([match_step, group_step, project_step, merge_step])
+
+
+if __name__ == "__main__":
+    _run_materialized_views_update(
+        reference_time=datetime(1970, 1, 1),
+        collection=get_activity_sessions_collection(),
+    )
