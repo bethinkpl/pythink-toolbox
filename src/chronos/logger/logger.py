888fc20529fd1221f579e6f9e9d4ee6d59de66e1
andrysiaczek <agata.and.11@gmail.com>
2020-09-25 15:24:12 +0200

    LACE-449 | add docstring

--- src/chronos/logger/logger.py
+++ src/chronos/logger/logger.py
@@ -4,5 +4,8 @@ from chronos.settings import SENTRY_DSN_API, LOG_LEVEL
 
 
 def init_for_api() -> None:
+    """
+    initializes API connection with sentry
+    """
     logging.getLogger(name="CHRONOS_API").setLevel(LOG_LEVEL)
     sentry_sdk.init(dsn=SENTRY_DSN_API)  # type: ignore[attr-defined]
