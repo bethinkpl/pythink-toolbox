24fea491226ba809c748571011a6ee81c7a2557c
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-25 15:30:24 +0200

    LACE-466 | conform UPPER_CASE naming style
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/settings.py
+++ src/chronos/settings.py
@@ -12,10 +12,10 @@ dotenv.load_dotenv(dotenv_path=ENV_PATH)
 
 BIGQUERY_PLATFORM_DATASET_ID: str = os.getenv("BIGQUERY_PLATFORM_DATASET_ID", "")
 
-prefix = chronos.__name__.upper()
+_SERVICE_NAME = chronos.__name__.upper()
 
-MONGO_HOST = os.getenv(f"{prefix}_MONGO_HOST", "")
-MONGO_PORT = int(os.getenv(f"{prefix}_MONGO_PORT", "0"))
-MONGO_USERNAME = os.getenv(f"{prefix}_MONGO_USER", "")
-MONGO_PASSWORD = os.getenv(f"{prefix}_MONGO_PASSWORD", "")
-MONGO_DATABASE = os.getenv(f"{prefix}_MONGO_DATABASE", "")
+MONGO_HOST = os.getenv(f"{_SERVICE_NAME}_MONGO_HOST", "")
+MONGO_PORT = int(os.getenv(f"{_SERVICE_NAME}_MONGO_PORT", "0"))
+MONGO_USERNAME = os.getenv(f"{_SERVICE_NAME}_MONGO_USER", "")
+MONGO_PASSWORD = os.getenv(f"{_SERVICE_NAME}_MONGO_PASSWORD", "")
+MONGO_DATABASE = os.getenv(f"{_SERVICE_NAME}_MONGO_DATABASE", "")
