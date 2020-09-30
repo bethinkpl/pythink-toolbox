18281c5041657e6c3f5ede6a3f0858bc6bafa004
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-30 18:27:38 +0200

    LACE-471 | collections & materialized_views properties added
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/storage.py
+++ src/chronos/storage.py
@@ -17,6 +17,49 @@ class MongoCommitError(StorageError):
     """Error that occurred while performing MongoDB commit."""
 
 
+@dataclass
+class _MaterializedView:
+    name: str
+    match_stage_conds: Dict[str, Any]
+
+    def __post_init__(self) -> None:
+        self.name = self.name + "_mv"
+
+    def update(self, reference_time: datetime) -> None:
+        """Updates materialized view content based on `reference time`."""
+        match_stage = {
+            **self.match_stage_conds,
+            "end_time": {"$gte": reference_time},
+        }
+
+        project_stage = {
+            "_id": {"user_id": "$user_id", "start_time": "$start_time"},
+            "end_time": 1,
+            "duration_ms": {"$sum": {"$subtract": ["$end_time", "$start_time"]}},
+        }
+
+        merge_stage = {"into": self.name, "whenMatched": "replace"}
+
+        mongodb.collections.activity_sessions.aggregate(
+            [
+                {"$match": match_stage},
+                {"$project": project_stage},
+                {"$merge": merge_stage},
+            ]
+        )
+
+
+class _MaterializedViewIDSchema(TypedDict):
+    user_id: int
+    start_time: datetime
+
+
+class MaterializedViewSchema(TypedDict):
+    _id: _MaterializedViewIDSchema
+    end_time: datetime
+    duration_ms: int
+
+
 class _MongoDB:
     def __init__(self) -> None:
         self._client: Optional[MongoClient] = None
@@ -48,73 +91,40 @@ class _MongoDB:
         return self._client[settings.MONGO_DATABASE]
 
     @property
-    def activity_sessions_collection(self) -> Collection:
+    def collections(self) -> dataclass:
         """
         Returns:
-            pymongo.collection.Collection
+            NamedTuple of collections.
         """
-        return self.database.activity_sessions
 
+        @dataclass
+        class Collections:
+            activity_sessions: Collection
+            user_generation_failed: Collection
 
-mongodb = _MongoDB()
+        return Collections(
+            activity_sessions=self.database.activity_sessions,
+            user_generation_failed=self.database.user_generation_failed,
+        )
 
-
-@dataclass
-class MaterializedView:
-    name: str
-    match_stage_conds: Dict[str, Any]
-
-    def __post_init__(self) -> None:
-        self.name = self.name + "_mv"
-
-    def update(self, collection: Collection, reference_time: datetime) -> None:
-        """Updates materialized view content based on `reference time`."""
-        match_stage = {
-            **self.match_stage_conds,
-            "end_time": {"$gte": reference_time},
-        }
-
-        project_stage = {
-            "_id": {"user_id": "$user_id", "start_time": "$start_time"},
-            "end_time": 1,
-            "duration_ms": {"$sum": {"$subtract": ["$end_time", "$start_time"]}},
-        }
-
-        merge_stage = {"into": self.name, "whenMatched": "replace"}
-
-        collection.aggregate(
-            [
-                {"$match": match_stage},
-                {"$project": project_stage},
-                {"$merge": merge_stage},
-            ]
+    @property
+    def materialized_views(self) -> Sequence[_MaterializedView]:
+        return (
+            _MaterializedView(
+                name="learning_time_sessions_duration",
+                match_stage_conds={
+                    "$or": [{"is_active": {"$eq": True}}, {"is_break": {"$eq": True}}]
+                },
+            ),
+            _MaterializedView(
+                name="break_sessions_duration",
+                match_stage_conds={"is_break": {"$eq": True}},
+            ),
+            _MaterializedView(
+                name="focus_sessions_duration",
+                match_stage_conds={"is_focus": {"$eq": True}},
+            ),
         )
 
 
-class MaterializedViewIDSchema(TypedDict):
-    user_id: int
-    start_time: datetime
-
-
-class MaterializedViewSchema(TypedDict):
-    _id: MaterializedViewIDSchema
-    end_time: datetime
-    duration_ms: int
-
-
-materialized_views: Sequence[MaterializedView] = (
-    MaterializedView(
-        name="learning_time_sessions_duration",
-        match_stage_conds={
-            "$or": [{"is_active": {"$eq": True}}, {"is_break": {"$eq": True}}]
-        },
-    ),
-    MaterializedView(
-        name="break_sessions_duration",
-        match_stage_conds={"is_break": {"$eq": True}},
-    ),
-    MaterializedView(
-        name="focus_sessions_duration",
-        match_stage_conds={"is_focus": {"$eq": True}},
-    ),
-)
+mongodb = _MongoDB()
