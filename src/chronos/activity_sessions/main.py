af0b1dbf671abaf597602acac00a7b51d60213bf
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-09-30 18:30:08 +0200

    LACE-471 | run extract_users_in_user_generation_failed_collection
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/activity_sessions/main.py
+++ src/chronos/activity_sessions/main.py
@@ -13,8 +13,14 @@ def main(start_time: datetime, end_time: datetime) -> None:
     """Create activity_sessions for all users
     who had activity_events between given timestamps."""
 
+    user_ids_to_be_excluded_in_activity_events_extraction = (
+        chronos.activity_sessions.storage_operations.extract_users_in_user_generation_failed_collection()
+    )
+
     users_activity_events = chronos.activity_sessions.activity_events_source.read_activity_events_between_datetimes(
-        start_time=start_time, end_time=end_time
+        start_time=start_time,
+        end_time=end_time,
+        exclude_user_ids=user_ids_to_be_excluded_in_activity_events_extraction,
     )
 
     logger.info(
@@ -24,6 +30,7 @@ def main(start_time: datetime, end_time: datetime) -> None:
     users_activity_events_groups = users_activity_events.groupby("user_id").client_time
 
     for user_id, activity_events in tqdm(users_activity_events_groups):
-        chronos.activity_sessions.storage_operations.main(
+
+        chronos.activity_sessions.storage_operations.save_new_activity_sessions(
             user_id, activity_events, reference_time=start_time
         )
