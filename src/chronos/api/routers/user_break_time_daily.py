ce239b53c615801644af9f3063bc49df134430e1
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-10-08 15:54:33 +0200

    LACE-495 | simplify types
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/api/routers/user_break_time_daily.py
+++ src/chronos/api/routers/user_break_time_daily.py
@@ -1,7 +1,8 @@
 from typing import List
+from datetime import datetime
 
-from dateutil.parser import isoparse
 from fastapi.routing import APIRouter
+from fastapi import Query
 
 from chronos.api.storage_operations import UserDailyTime, read_daily_break_time
 
@@ -10,14 +11,20 @@ user_break_time_daily_router = APIRouter()
 
 @user_break_time_daily_router.get("/user_break_daily_router/{user_id}")
 def get_user_break_time_daily(
-    user_id: int, start_time: str, end_time: str
+    user_id: int,
+    start_time: datetime = Query(
+        ..., alias="start-time", title="", description=""
+    ),  # TODO LACE-469 fill when documenting
+    end_time: datetime = Query(
+        ..., alias="end-time", title="", description=""
+    ),  # TODO LACE-469 fill when documenting
 ) -> List[UserDailyTime]:
     """
     API end-point | Provides user's daily break time.
     """
-    break_time = read_daily_break_time(
+
+    return read_daily_break_time(
         user_id=user_id,
-        start_time=isoparse(start_time),
-        end_time=isoparse(end_time),
+        start_time=start_time,
+        end_time=end_time,
     )
-    return break_time
