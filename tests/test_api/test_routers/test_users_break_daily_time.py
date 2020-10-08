a351d266d0023016cf64bb141a6b3402515d137e
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-10-08 18:44:49 +0200

    LACE-495 | adjust test to current implemented state
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- tests/test_api/test_routers/test_users_break_daily_time.py
+++ tests/test_api/test_routers/test_users_break_daily_time.py
@@ -1,4 +1,4 @@
-import json
+from datetime import datetime
 
 import pytest
 import requests
@@ -6,20 +6,20 @@ import requests
 from tests.consts import HEADERS, STATUS_OK
 from tests.test_api.url_helpers import get_url
 
-TEST_DATA = {"start_time": 15934399493, "end_time": 15934399490}
+TEST_DATA = {
+    "range-start": datetime(2000, 1, 1).isoformat(),
+    "range-end": datetime(2021, 1, 1).isoformat(),
+}
 
 USER_ID = 299
 
 
+@pytest.mark.e2e
+@pytest.mark.integration
 @pytest.mark.skip(reason="https://bethink.atlassian.net/browse/LACE-465")  # type: ignore
-def test_get_user_break_time_daily() -> None:
-    """
-    Covers src.test_api.user_break_time_daily.get_user_break_time_daily()
-    """
-    response = requests.post(
-        get_url(f"break_time_daily/{USER_ID}"),
-        headers=HEADERS,
-        data=json.dumps(TEST_DATA),
+def test_get_users_break_daily_time() -> None:
+    response = requests.get(
+        get_url(f"users_break_daily_time/{USER_ID}"), headers=HEADERS, params=TEST_DATA
     )
     assert response.status_code == STATUS_OK
     assert response.json() == {"2000-01-01": 200, "2000-01-02": 400, "2000-01-03": 50}
---
+++ tests/test_api/test_routers/test_users_cumulative_learning_time.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+import pytest
+import requests
+
+from tests.consts import HEADERS, STATUS_OK
+from tests.test_api.url_helpers import get_url
+
+TEST_DATA = {
+    "range-start": datetime(2000, 1, 1).isoformat(),
+    "range-end": datetime(2021, 1, 1).isoformat(),
+}
+
+USER_ID = 299
+
+
+@pytest.mark.e2e
+@pytest.mark.integration
+@pytest.mark.skip(reason="https://bethink.atlassian.net/browse/LACE-465")  # type: ignore
+def test_get_users_cumulative_learning_time() -> None:
+    response = requests.get(
+        get_url(f"users_cumulative_learning_time/{USER_ID}"),
+        headers=HEADERS,
+        params=TEST_DATA,
+    )
+    assert response.status_code == STATUS_OK
+    assert response.json() == 650
