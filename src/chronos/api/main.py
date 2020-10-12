160e80a6f62b0d9673d5e5069c4937b3b75ccf00
Kuba Karmiński <jlkarminski@gmail.com>
2020-10-12 16:20:41 +0200

    Fix the api container config

--- src/chronos/api/main.py
+++ src/chronos/api/main.py
@@ -1,5 +1,5 @@
-import uvicorn
 from fastapi.applications import FastAPI
+
 from chronos.api.routers import (
     user_break_time_daily,
     user_focus_time_daily,
@@ -8,7 +8,6 @@ from chronos.api.routers import (
     users_learning_time,
 )
 from chronos.logger import logger
-from chronos.settings import HOST_API
 
 logger.init_for_api()
 app = FastAPI()
@@ -18,9 +17,3 @@ app.include_router(user_focus_time_daily.focus_daily_router)
 app.include_router(user_learning_time.user_learning_router)
 app.include_router(user_learning_time_daily.learning_daily_router)
 app.include_router(users_learning_time.users_learning_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "chronos.api.main:app", host=HOST_API, port=5000, debug=True, reload=True
-    )
--- src/chronos/settings.py
+++ src/chronos/settings.py
@@ -3,22 +3,17 @@ import os
 
 import dotenv
 
-import chronos
-
-
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 BIGQUERY_PLATFORM_DATASET_ID: str = os.getenv("BIGQUERY_PLATFORM_DATASET_ID", "")
 
-_SERVICE_NAME = chronos.__name__.upper()
+LOG_LEVEL = os.getenv("CHRONOS_LOG_LEVEL", logging.getLevelName(logging.INFO))
+SENTRY_DSN_API = os.getenv("CHRONOS_SENTRY_DSN_API")
 
-LOG_LEVEL = os.getenv(f"{_SERVICE_NAME}_LOG_LEVEL", logging.getLevelName(logging.INFO))
-SENTRY_DSN_API = os.getenv(f"{_SERVICE_NAME}_SENTRY_DSN_API")
+MONGO_HOST = os.getenv("CHRONOS_MONGO_HOST", "")
+MONGO_PORT = int(os.getenv("CHRONOS_MONGO_PORT", "0"))
+MONGO_USERNAME = os.getenv("CHRONOS_MONGO_USER", "")
+MONGO_PASSWORD = os.getenv("CHRONOS_MONGO_PASSWORD", "")
+MONGO_DATABASE = os.getenv("CHRONOS_MONGO_DATABASE", "")
 
-MONGO_HOST = os.getenv(f"{_SERVICE_NAME}_MONGO_HOST", "")
-MONGO_PORT = int(os.getenv(f"{_SERVICE_NAME}_MONGO_PORT", "0"))
-MONGO_USERNAME = os.getenv(f"{_SERVICE_NAME}_MONGO_USER", "")
-MONGO_PASSWORD = os.getenv(f"{_SERVICE_NAME}_MONGO_PASSWORD", "")
-MONGO_DATABASE = os.getenv(f"{_SERVICE_NAME}_MONGO_DATABASE", "")
-
-HOST_API = os.getenv(f"{_SERVICE_NAME}_HOST_API")
+HOST_API = os.getenv("CHRONOS_HOST_API", "localhost")
