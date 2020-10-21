d1550075dd48444d4ec18ac0d34f1c6c50a4b444
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-10-21 18:33:02 +0200

    LACE-479 | rename
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

---
+++ src/chronos/storage/mongo_specs.py
@@ -0,0 +1,120 @@
+from collections import namedtuple
+import json
+from typing import Dict, Sequence, Any
+from datetime import datetime
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+from chronos import settings
+
+
+_MaterializedViewConf = namedtuple(
+    "MaterializedViewConf", ["name", "match_stage_conds"]
+)
+
+
+class _MaterializedView(Collection):  # type: ignore[misc]
+    def __init__(
+        self,
+        name: str,
+        match_stage_conds: Dict[str, Any],
+        database_: Database,
+        **kwargs,
+    ):
+        super().__init__(database=database_, name=name, **kwargs)
+        self.match_stage_conds = match_stage_conds
+
+    def run_aggregation(self, collection: Collection, reference_time: datetime):
+        """Updates materialized view content based on `reference time`."""
+        match_stage = {
+            **self.match_stage_conds,
+            "end_time": {"$gte": reference_time},
+        }
+
+        project_stage = {
+            "_id": {"user_id": "$user_id", "start_time": "$start_time"},
+            "end_time": 1,
+            "duration_ms": {"$sum": {"$subtract": ["$end_time", "$start_time"]}},
+        }
+
+        merge_stage = {"into": self.name, "whenMatched": "replace"}
+
+        collection.aggregate(
+            [
+                {"$match": match_stage},
+                {"$project": project_stage},
+                {"$merge": merge_stage},
+            ]
+        )
+
+
+def _initialize_collections(
+    collections_names: Sequence[str], database_: Database
+) -> Dict[str, Collection]:
+
+    for collection_name in collections_names:
+        if collection_name not in database_.list_collection_names():
+            validator_file_path = (
+                settings.CHRONOS_PACKAGE_DIR
+                / f"storage/schema_validators/{collection_name}.json"
+            )
+
+            with open(validator_file_path, "r") as file:
+                validator = json.loads(file.read())
+
+            database_.create_collection(collection_name, validator=validator)
+
+    return {
+        collection_name: database_.get_collection(name=collection_name)
+        for collection_name in collections_names
+    }
+
+
+def _initialize_materialized_views(
+    materialized_views_confs: Sequence[_MaterializedViewConf], database_: Database
+):
+    return {
+        materialized_view_conf.name: _MaterializedView(
+            name=materialized_view_conf.name,
+            match_stage_conds=materialized_view_conf.match_stage_conds,
+            database_=database_,
+        )
+        for materialized_view_conf in materialized_views_confs
+    }
+
+
+client = MongoClient(
+    host=settings.MONGO_HOST,
+    port=settings.MONGO_PORT,
+    username=settings.MONGO_USERNAME,
+    password=settings.MONGO_PASSWORD,
+)
+
+database = client[settings.MONGO_DATABASE]
+
+collections = _initialize_collections(
+    ["activity_sessions", "user_generation_failed", "generations"],
+    database_=database,
+)
+
+materialized_views = _initialize_materialized_views(
+    materialized_views_confs=(
+        _MaterializedViewConf(
+            name="learning_time_sessions_duration_mv",
+            match_stage_conds={
+                "$or": [{"is_active": {"$eq": True}}, {"is_break": {"$eq": True}}]
+            },
+        ),
+        _MaterializedViewConf(
+            name="break_sessions_duration_mv",
+            match_stage_conds={"is_break": {"$eq": True}},
+        ),
+        _MaterializedViewConf(
+            name="focus_sessions_duration_mv",
+            match_stage_conds={"is_focus": {"$eq": True}},
+        ),
+    ),
+    database_=database,
+)
