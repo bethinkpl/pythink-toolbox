46ca11558573f38ea2c6db7c02da60beb5969955
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-10-21 18:23:47 +0200

    LACE-479 | update activity sessions generation with generation times
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/activity_sessions/main.py
+++ src/chronos/activity_sessions/main.py
@@ -3,23 +3,32 @@ import logging
 
 from tqdm import tqdm
 
-import chronos.activity_sessions.storage_operations
+from chronos.activity_sessions import storage_operations, activity_events_source
 
 logger = logging.getLogger(__name__)
 
 
-def main(start_time: datetime, end_time: datetime) -> None:
-    """Create activity_sessions for all users
-    who had activity_events between given timestamps."""
+def main(time_range: storage_operations.TimeRange) -> None:
+    """Create activity_sessions for all the users
+    who had activity_events in a given time range
+    and update the materialized views."""
 
-    user_ids_to_be_excluded_in_activity_events_extraction = (
-        chronos.activity_sessions.storage_operations.extract_users_in_user_generation_failed_collection()
+    generation_start_time = datetime.now()
+
+    generation_id = storage_operations.insert_new_generation(
+        time_range=time_range, start_time=generation_start_time
     )
 
-    users_activity_events = chronos.activity_sessions.activity_events_source.read_activity_events_between_datetimes(
-        start_time=start_time,
-        end_time=end_time,
-        exclude_user_ids=user_ids_to_be_excluded_in_activity_events_extraction,
+    user_ids_to_be_excluded_in_activity_events_extraction = (
+        storage_operations.extract_users_in_user_generation_failed_collection()
+    )
+
+    users_activity_events = (
+        activity_events_source.read_activity_events_between_datetimes(
+            start_time=time_range.start,
+            end_time=time_range.end,
+            exclude_user_ids=user_ids_to_be_excluded_in_activity_events_extraction,
+        )
     )
 
     logger.info(
@@ -30,10 +39,15 @@ def main(start_time: datetime, end_time: datetime) -> None:
 
     for user_id, activity_events in tqdm(users_activity_events_groups):
 
-        chronos.activity_sessions.storage_operations.save_new_activity_sessions(
-            user_id, activity_events, reference_time=start_time
+        storage_operations.save_new_activity_sessions(
+            user_id, activity_events, reference_time=time_range.start
         )
 
-        chronos.activity_sessions.storage_operations.update_materialized_views(
-            reference_time=start_time
-        )
+    storage_operations.update_materialized_views(reference_time=time_range.start)
+
+    generation_end_time = datetime.now()
+    storage_operations.update_generation_end_time(
+        generation_id=generation_id, end_time=generation_end_time
+    )
+
+    logger.info("Generation took %s", generation_end_time - generation_start_time)
