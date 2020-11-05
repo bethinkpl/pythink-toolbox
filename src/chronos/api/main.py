715e788d49c7c5faa5f655449552fec5ec1116be
andrysiaczek <agata.and.11@gmail.com>
2020-11-05 12:55:33 +0100

    LACE-496-API_documentation | mypy fix with custom_openapi

--- src/chronos/api/main.py
+++ src/chronos/api/main.py
@@ -1,6 +1,8 @@
-from fastapi.applications import FastAPI
+from typing import Dict, Any
 
-from chronos.api.generate_api_doc import custom_openapi
+from fastapi.applications import FastAPI
+from fastapi.openapi.utils import get_openapi
+
 from chronos.api.routers.users_break_daily_time import users_break_daily_time_router
 from chronos.api.routers.users_cumulative_learning_time import (
     users_cumulative_learning_time_router,
@@ -10,6 +12,7 @@ from chronos.api.routers.users_learning_daily_time import (
     users_learning_daily_time_router,
 )
 from chronos.logger import logger
+from chronos import __version__
 
 logger.init_for_api()
 app = FastAPI()
@@ -18,5 +21,22 @@ app.include_router(users_break_daily_time_router)
 app.include_router(users_focus_daily_time_router)
 app.include_router(users_learning_daily_time_router)
 app.include_router(users_cumulative_learning_time_router)
+
+
+def custom_openapi() -> Dict[str, Any]:
+    """
+    Add custom description to openapi schema.
+    """
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Chronos API",
+        version=__version__,
+        description="Chronos FastAPI schema",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 setattr(app, "openapi", custom_openapi)  # https://github.com/python/mypy/issues/2427
