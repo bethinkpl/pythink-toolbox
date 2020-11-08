64efea8b62602d30253b73b9432a885b20b98b6a
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-11-08 14:11:38 +0100

    LACE-514 | simplify read_activity_events_between_datetimes
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

--- src/chronos/activity_sessions/activity_events_source.py
+++ src/chronos/activity_sessions/activity_events_source.py
@@ -8,34 +8,29 @@ from chronos.settings import BIGQUERY_PLATFORM_DATASET_ID
 
 
 def read_activity_events_between_datetimes(
-    start_time: Union[datetime, str],
-    end_time: Union[datetime, str],
-    exclude_user_ids: Optional[List[int]] = None,
+    start_time: datetime,
+    end_time: datetime,
+    user_ids: Optional[List[int]] = None,
+    exclude: bool = False,
 ) -> pd.DataFrame:
-    """
-    Read activity events from bigquery
-    """
+    """Read activity events from bigquery."""
 
     bigquery_source = datatosk.gbq("prod")
-
-    params: Dict[str, Union[datetime, str, List[int]]] = {
-        "start_time": start_time,
-        "end_time": end_time,
-    }
-
-    exclude_user_ids_condition = ""
-    if exclude_user_ids:
-        exclude_user_ids_condition = "AND user_id NOT IN @exclude_user_ids"
-        params["exclude_user_ids"] = exclude_user_ids
 
     query = f"""
         SELECT user_id, DATETIME(client_time) as client_time
         FROM `{BIGQUERY_PLATFORM_DATASET_ID}.user_activity_events`
         WHERE client_time >= @start_time
           AND client_time < @end_time
-          {exclude_user_ids_condition}
+          AND user_id{" NOT " if exclude else " "}IN @user_ids
         GROUP BY user_id, client_time
         ORDER BY user_id, client_time;
         """
+
+    params: Dict[str, Union[datetime, List[int]]] = {
+        "start_time": start_time,
+        "end_time": end_time,
+        "user_ids": user_ids,
+    }
 
     return bigquery_source.read(query=query, params=params)
