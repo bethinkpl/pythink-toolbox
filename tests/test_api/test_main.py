9e2d2a1e2a4e870f799aabd68573f30ec0c0f814
miloszbednarzak <milosz.bednarzak@gmail.com>
2020-12-04 16:33:13 +0100

    LACE-514 | add api health check test
    Signed-off-by: miloszbednarzak <milosz.bednarzak@gmail.com>

---
+++ tests/test_api/test_main.py
@@ -0,0 +1,13 @@
+from typing import Dict
+
+from starlette.testclient import TestClient
+
+
+def test_health(
+    api_client: TestClient, status_ok: int, headers: Dict[str, str]
+) -> None:
+
+    response = api_client.get(f"/health", headers=headers)
+
+    assert response.status_code == status_ok
+    assert response.json() == "I'm alive"
